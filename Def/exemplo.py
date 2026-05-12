45539189682141e84718274bff315570d6342343
Carlos Daniel <carlos123danielsouza@gmail.com>
2026-05-12 12:18:14 -0300

    add new

---
+++ Def/exemplo.py
@@ -0,0 +1,12 @@
+
+def saudacao(nome):
+
+    return f"Olá, {nome}!"
+
+def dobro(x):
+
+    return x * 2
+
+def calcular_media(n1, n2):
+
+    return (n1 + n2) / 2
---
+++ Def/uso.py
@@ -0,0 +1,5 @@
+import exemplo 
+
+print(exemplo.saudacao("Alice"))
+print(exemplo.dobro(5))
+print(exemplo.calcular_media(8, 10))
